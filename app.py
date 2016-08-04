4c7e6d7aca145f9f7a4dc21e60e8a065450839ae
Robert Powers <robert.powers@gdt.com>
2016-08-04 16:19:59 -0500

    Add App code

---
+++ app.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import ast
+# -- Possible status values are JobRunning, JobFinished, JobStopped, JobError, NodeStarting, NodeReady, NodeTerminated --
+
+def get_all_jobs():
+    r = requests.get('http://127.0.0.1:5000/v1/jobs')
+    return r.text
+
+def get_job(jobId):
+    print 'http://127.0.0.1:5000/v1/jobs/{0}'.format(jobId)
+    r = requests.get('http://127.0.0.1:5000/v1/jobs/{0}'.format(jobId))
+    return r.text
+
+def eval_job_status(jobId):
+    return NotImplementedError
+
+def str_to_dict(text):
+    # Used for debugging, since we don't have access to a cliqr appliance yet to verify output
+    return ast.literal_eval(text)
+
+if __name__ == "__main__":
+    # Get all jobs currently running
+    jobList = get_all_jobs()
+
+    # Take this out when we're done debugging
+    jobList = str_to_dict(jobList)
+
+    # For every job in the list, get details and update status
+    print jobList["jobs"]
+    
+    #for job in jobList["jobs"]:
+    #    print job
+    #jobList["jobs"][0]["id"]
+    
