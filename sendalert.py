a4dc9647b7ac716a796d1ff9b053ee1223155ef6
hunter3of13 <Hunter@Hunters-MacBook-Pro.local>
2016-08-05 09:50:53 -0500

    Fixed Email Subject issue
    Added MIMEText package to reformat body of email so Emails will be sent
    correctly.

--- sendalert.py
+++ sendalert.py
@@ -1,31 +1,37 @@
 import smtplib
-from email.mime.text import MIMEText as text
+from email.mime.text import MIMEText
 import base64
 
+"""
+Change Sendter Email Address.
+Change Receiver address.
+Change the Subject to what you need it to me.
+
+Enter the login info for the SMTP Server.
+"""
+
+
 def send_email_alert(msg):
-    
-    sender = 'cliqr@cliqrtech.com'
-    receiver = 'hunter.kaemmerling@gdt.com'
-    subject = "Something really bad broke somewhere"
-    Password = base64.b64decode("Z2xvY2sxNzIz")
-    server = smtplib.SMTP('smtp.1and1.com')
+    msg = MIMEText(msg)
+    sender = 'WHO IS THIS EMAIL COMING FROM'
+    receiver = 'ENTER THE EMAIL ADDRESS YOU WANT TO SEND THIS EMAIL TOO'
+    subject = 'ENTER SUBJECT'
+    Username = 'ENTER THE EMAIL USER NAME'
+    Password = 'EMAIL Account Password'
+    server = smtplib.SMTP('SMTP HOST')
     server.starttls()
     #server.set_debuglevel(1)
-    #server.ehlo_or_helo_if_needed()
-
-    message = """From: %s
-    To: %s
-    Subject: %s
-    %s 
-    """ % (sender, receiver, subject, msg)
+    server.ehlo_or_helo_if_needed()
 
 
-    server.login("hunter@mykcrew.net",Password)
-    server.sendmail(sender, receiver, message)
-    #print smtplib.SMTPAuthenticationError
-    #print smtplib.SMTPConnectError
-    #print smtplib.SMTPException
-    #print smtplib.SMTPHeloError
+    # me == the sender's email address
+    # you == the recipient's email address
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = receiver
+
+    #print msg.as_string()
+
+    server.login(Username,Password)
+    server.sendmail(sender, [receiver], msg.as_string())
     server.quit()
-
-
